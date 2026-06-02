b2fc59c21e43fa4997a89ab4b916bbea513cacf4
azuuulsg <azuuusgl@gmail.com>
2026-06-02 08:24:23 -0300

    servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Contacto } from './paginas/contacto/contacto';
+import { Ofertas } from './paginas/ofertas/ofertas';
+import { Productos } from './paginas/productos/productos';
+import { Carrito } from './compartidos/carrito/carrito';
+import { Favoritos } from './compartidos/favoritos/favoritos';
+import { Registro } from './auth/registro/registro';
+import { Iniciosesion } from './auth/iniciosesion/iniciosesion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: 'Inicio', pathMatch: 'full'},
+
+    { path: 'Inicio', component: Inicio},
+    { path: 'Contacto', component: Contacto},
+    { path: 'Ofertas', component: Ofertas},
+    { path: 'Productos', component: Productos},
+    { path: 'Carrito', component: Carrito},
+    { path: 'Favoritos', component: Favoritos},
+    { path: 'Registro', component: Registro},
+    { path: 'Iniciosesion', component: Iniciosesion},
+
+
+    {path: '**', redirectTo: 'Inicio'},
+];
--- src/app/paginas/productos/productos.ts
+++ src/app/paginas/productos/productos.ts
@@ -1,9 +1,77 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../../models/productos';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './productos.html',
   styleUrl: './productos.css',
 })
-export class Productos {}
+export class Productos {
+  Productos: Producto[] = [
+    {
+      id: 1,
+      nombre: "Corrector",
+      descripcion: "Corrector de ojeras",
+      precio: 4500,
+      stock: 4,
+      imagen: "https://i.pinimg.com/736x/11/d8/be/11d8be89cdebd8eaa77b26dd70067e8e.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    },
+    {
+      id: 2,
+      nombre: "Rimel",
+      descripcion: "Rimel de pestañas",
+      precio: 5500,
+      stock: 2,
+      imagen: "https://i.pinimg.com/736x/f0/da/13/f0da13f060bd17b9d3b5fdf243fb449c.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    },
+    {
+      id: 3,
+      nombre: "Rubor Dior",
+      descripcion: "Rubor Rosado Dior",
+      precio: 3300,
+      stock: 4,
+      imagen: "https://i.pinimg.com/736x/67/1e/ab/671eabb572e5002e82fe90c3eb9f9aba.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    },
+    {
+      id: 4,
+      nombre: "Labial KiKo",
+      descripcion: "Labial liquido Kiko",
+      precio: 6500,
+      stock: 7,
+      imagen: "https://i.pinimg.com/736x/2c/53/30/2c5330a9cb297c3dab62d0ab9e788b5e.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    },
+    {
+      id: 5,
+      nombre: "Polvo compacto",
+      descripcion: "Polvo compacto Dior",
+      precio: 6600,
+      stock: 1,
+      imagen: "https://i.pinimg.com/1200x/05/21/f8/0521f8c1c3c7e7ec2fdbd79305b2b15b.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    },
+    {
+      id: 6,
+      nombre: "Sombras de ojos",
+      descripcion: "Paleta  de sombras rosa",
+      precio: 4500,
+      stock: 11,
+      imagen: "https://i.pinimg.com/736x/73/2b/12/732b12c99b46df2b06f5be1badea003d.jpg",
+      categoria: "almacen",
+      disponibilidad: true,
+    }
+
+  ]
+
+}
+
